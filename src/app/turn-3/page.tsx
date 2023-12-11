03e2f8a0dde02f3490ea7390b0c4bec4bcee816e
vzhytnyk <vzhytnyk@redrocket.software>
2023-12-11 13:57:02 +0200

    fixed issue with add

--- src/app/turn-3/page.tsx
+++ src/app/turn-3/page.tsx
@@ -6,9 +6,11 @@ import gameConfigActions from '@/redux/gameConfig/gameConfig.actions';
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import styles from '../page.module.css';
+import useMediaQuery from '@/hooks/useMediaQuery';
 
 export default function Home() {
   const dispatch = useDispatch();
+  const isDesktop = useMediaQuery('(width >= 1024px)');
 
   useEffect(() => {
     dispatch(gameConfigActions.setGameMode('turnThree'));
@@ -17,12 +19,14 @@ export default function Home() {
   return (
     <main>
       <div className={styles.boardWithAdsContainer}>
-        <div id="game-board-container" className={styles.boardContainer}>
+        <div id='game-board-container' className={styles.boardContainer}>
           <GameBoard />
         </div>
-        <div className={styles.adsContainer}>
-          <AdsBlock />
-        </div>
+        {isDesktop && (
+          <div className={styles.adsContainer}>
+            <AdsBlock />
+          </div>
+        )}
       </div>
       <GameInfo />
     </main>
